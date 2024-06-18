8ef9d06f81896c493dab9f97dc87f989de866f64
bhkh007 <khandarbhavesh007@gmail.com>
2024-06-18 20:26:46 +0530

    second python program

---
+++ pythonProject/secondpython.py
@@ -0,0 +1,19 @@
+
+a="123"
+b=5.5
+a=float(a)
+print(type(a))
+print(a+b)
+
+
+age =  int(input("Enter your age: "))
+print(age)
+print(type(age))
+
+marks = float(input("Enter marks: "))
+print(type(marks))
+print(age+marks)
+
+
+test = eval(input("Enter your concern: "))
+print(test)
