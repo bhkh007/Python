c8589b527e8059b4a0b826ebe6db94e9f6bfe6ac
bhkh007 <khandarbhavesh007@gmail.com>
2024-06-18 19:24:41 +0530

    first python program

---
+++ pythonProject/firstpython.py
@@ -0,0 +1,13 @@
+
+
+print ("First Python Program")
+print("Second line")
+
+print("""Third Line
+Fourth Line
+Fifth Line""")
+
+print("Today is Ind VS Pak match\nToday is Ind VS Aus match\nToday is Ind VS WI match")
+
+#Ignore this comment
+"""This one also"""
